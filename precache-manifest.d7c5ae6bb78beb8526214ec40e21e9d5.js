06129d8034e30820e044206f3f88c6038d1a35ed
kacperjagiela <jagielakacper80@gmail.com>
2019-08-08 11:31:55 +0200

    Updates

---
+++ precache-manifest.d7c5ae6bb78beb8526214ec40e21e9d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0018cf7c486d395407e0127a019a46fc",
+    "url": "/qa-app-mongodb/index.html"
+  },
+  {
+    "revision": "0d6ef3bcd0dacdd2c8e2",
+    "url": "/qa-app-mongodb/static/css/2.ab7e5612.chunk.css"
+  },
+  {
+    "revision": "306f477c10bb981d4391",
+    "url": "/qa-app-mongodb/static/css/main.7aa1ebcb.chunk.css"
+  },
+  {
+    "revision": "0d6ef3bcd0dacdd2c8e2",
+    "url": "/qa-app-mongodb/static/js/2.f331f2bc.chunk.js"
+  },
+  {
+    "revision": "306f477c10bb981d4391",
+    "url": "/qa-app-mongodb/static/js/main.9806d0e1.chunk.js"
+  },
+  {
+    "revision": "126b60e9a4deb9be21b8",
+    "url": "/qa-app-mongodb/static/js/runtime~main.a6c04878.js"
+  }
+]);
